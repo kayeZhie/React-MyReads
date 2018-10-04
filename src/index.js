bfd3b21336426979134cd84a9c654c6e31a2d48e
Karren Zoleta <zoletakarren@gmail.com>
2018-10-03 18:16:13 -0700

    removed hardcoded data/update book shelf and book

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import {BrowserRouter} from 'react-router-dom'
+import BooksApp from './App'
 import './index.css'
 
-import { BrowserRouter} from 'react-router-dom'
-
-//route component inside browser router//
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, 
-	document.getElementById('root'))
+ReactDOM.render(
+<BrowserRouter>
+  <BooksApp/>
+</BrowserRouter>, document.getElementById('root'))
